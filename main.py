854da74bb619480b2e36fd527e02ccfe160e7260
Gitansh Raj Satija <55681531+gitongithub@users.noreply.github.com>
2021-05-25 14:30:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,67 +7,76 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from tqdm import tqdm
 import numpy as np
-# Mention the language keyword
-def func(author,s):
-    line=s
-    tool = language_tool_python.LanguageTool('en-US')
-    i = 0
-    c=0
-    errors={}
-    matches = tool.check(line)
-    print(matches)
-    i = i + len(matches)
-    for mistake in matches:
-        c += 1
-        errors[c] = {'Error': mistake.ruleId, 'Suggestion': mistake.replacements,'Message':mistake.message, 'Actual': mistake.matchedText}
-    print("No. of mistakes found in document is ", i)
-    print()
-    y = json.dumps(errors)
-    print(y)
-    f = open(author+".txt", "w+")
-    f.write("Submission by: "+author)
-    f.write("\n\nYour Submission: "+line)
-    f.write("\n\nNo. of mistakes found in submission is "+str(i))
-    for j in errors:
-        temp="\n\nError message: "+str(errors[j]['Message'])+"\nType of error: "+str(errors[j]['Error']) +"\nSuggestion is: "+str(errors[j]['Suggestion'])+"\nMistake found in: "+str(errors[j]['Actual'])
-        f.write(temp)
+
+
+def findError(author, msgList,questions):
+    f = open(author + ".txt", "w+")
+    f.write("Submission by: " + author)
+    ans=[]
+    for i in msgList:
+        temp1=i.split(" ",1)
+        temp2=temp1[0].split("A")
+        temp3=temp2[1]
+        ans.append(int(temp3))
+    temp="\n\nTotal Number of Assignments = " +str(len(questions)) + "\nNumber of Assignments Attempted = " + str(len(ans))
+    f.write(temp)
+    cnt=0
+    for s in range(len(questions)):
+        string="\n\nAssignment "+str(s+1)+":" + questions[s].split(" ",1)[1]
+        f.write(string)
+        if s+1 in ans:
+            line = msgList[cnt].split(" ",1)[1]
+            cnt+=1
+            tool = language_tool_python.LanguageTool('en-US')
+            i = 0
+            c = 0
+            errors = {}
+            matches = tool.check(line)
+            i = i + len(matches)
+            for mistake in matches:
+                c += 1
+                errors[c] = {'Error': mistake.ruleId, 'Suggestion': mistake.replacements, 'Message': mistake.message,
+                             'Actual': mistake.matchedText}
+
+            f.write("\n\nYour Submission: " + line)
+            f.write("\n\nNo. of mistakes found in submission is " + str(i))
+            for j in errors:
+                temp = "\n\nError message: " + str(errors[j]['Message']) + "\nType of error: " + str(
+                    errors[j]['Error']) + "\nSuggestion is: " + str(errors[j]['Suggestion']) + "\nMistake found in: " + str(
+                    errors[j]['Actual'])
+                f.write(temp)
+        else:
+            f.write("\n\nYou did not attempt this assignment.")
+        f.write("\n----------x----------x---------")
     f.close()
+
+
 def split_text(filename):
-    """
-    Split file contents by newline.
-    """
     chat = open(filename, encoding="utf8")
     chatText = chat.read()
     return chatText.splitlines()
 
+
 def distributeByAmPm(linesText):
     timeRegex = re.compile("\d+\/\d+\/\d+, (\d+\:\d+)")
-
     AM, PM = [], []
     for index, line in tqdm(enumerate(linesText)):
         matches = re.findall(timeRegex, line)
         if (len(matches) > 0):
             match = datetime.datetime.strptime(
                 matches[0], "%H:%M").strftime('%p')
-
             if match == "AM":
                 AM.append(matches[0])
             else:
                 PM.append(matches[0])
-
     return AM, PM
 
 
 def getDataPoint(line):
-
     splitLine = line.split(' - ')
-
     dateTime = splitLine[0]
-
     date, time = dateTime.split(', ')
-
     message = ' '.join(splitLine[1:])
-
     if startsWithAuthor(message):
         splitMessage = message.split(': ')
         author = splitMessage[0]
@@ -76,17 +85,18 @@ def getDataPoint(line):
         author = None
     return date, time, author, message
 
+
 def typeOfMessage(message):
     qPattern = 'Q[0-9]{1,2}'
     aPattern = 'A[0-9]{1,2}'
     if re.match(qPattern, message):
-        print("True")
         return 0
     elif re.match(aPattern, message):
-        print("False")
         return 1
     else:
         return -1
+
+
 def startsWithAuthor(s):
     patterns = [
         '([\w]+):',
@@ -102,6 +112,7 @@ def startsWithAuthor(s):
         return True
     return False
 
+
 def startsWithDateTime(s):
     pattern = '^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)(\d{2}|\d{4}), ([0-9][0-9]):([0-9][0-9]) -'
     result = re.match(pattern, s)
@@ -111,15 +122,14 @@ def startsWithDateTime(s):
 
 
 if __name__ == '__main__':
-    parsedData = []
     parsedData2 = []
-    conversationPath = "WhatsApp Chat with Induction 20' Volunteers.txt"
+    questions=[]
+    authorsList ={}
+    conversationPath = "iSaksham Test.txt"
     with open(conversationPath, encoding="utf-8") as fp:
         fp.readline()
-
         messageBuffer = []
         date, time, author = None, None, None
-
         while True:
             line = fp.readline()
             if not line:
@@ -127,35 +137,45 @@ if __name__ == '__main__':
             line = line.strip()
             if startsWithDateTime(line):
                 if len(messageBuffer) > 0:
-                    value=typeOfMessage(' '.join(messageBuffer))
-                    if value==0:
-                        parsedData.append([date, time, author, ' '.join(messageBuffer)])
-                    elif value==1:
+                    value = typeOfMessage(' '.join(messageBuffer))
+                    if value == 0:
+                        questions.append(' '.join(messageBuffer))
+                    elif value == 1:
                         parsedData2.append([date, time, author, ' '.join(messageBuffer)])
+                        if author in authorsList.keys():
+                            authorsList[author] = authorsList[author] + 1
+                        else:
+                            authorsList[author] = 1
                 messageBuffer.clear()
                 date, time, author, message = getDataPoint(line)
                 messageBuffer.append(message)
             else:
                 messageBuffer.append(line)
-    df = pd.DataFrame(parsedData, columns=['Date', 'Time', 'Author', 'Message'])
-    df.head()
-    print(df)
+    #print(questions)
     df2 = pd.DataFrame(parsedData2, columns=['Date', 'Time', 'Author', 'Message'])
     df2.head()
-    print(df2)
+    #print(df2)
     desired_width = 320
     pd.set_option('display.width', desired_width)
     np.set_printoptions(linewidth=desired_width)
     pd.set_option('display.max_columns', 10)
     pd.set_option('display.max_rows', None)
-    df = df.sort_values(by='Author')
-    df = df[df.Author.notnull()]
-    media_messages_df = df[df['Message'] == '<Media omitted>']
-    df = df.drop(media_messages_df.index)
-    author="Aniansh"
-    authordf = df.loc[df['Author'] == author]
-    value = str(authordf.iloc[-1].Message)
-    print(value)
-    #func(author,value)
-
+    df2 = df2.sort_values(by='Author')
+    df2 = df2[df2.Author.notnull()]
+    media_messages_df = df2[df2['Message'] == '<Media omitted>']
+    df2 = df2.drop(media_messages_df.index)
+    for i in authorsList:
+        authordf = df2.loc[df2['Author'] == i]
+        msgList=[]
+        for index, row in authordf.iterrows():
+            value = str(row['Message'])
+            msgList.append(value)
+        findError(i, msgList,questions)
+    # i='Aryan Taneja'
+    # authordf = df2.loc[df2['Author'] == i]
+    # msgList=[]
+    # for index, row in authordf.iterrows():
+    #     value = str(row['Message'])
+    #     msgList.append(value)
+    # findError(i, msgList,questions)
 
